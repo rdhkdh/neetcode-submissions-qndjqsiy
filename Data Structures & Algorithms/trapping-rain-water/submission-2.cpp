0c1a07e405604140496fa12388f2c8e1e7944c6b
Ridhiman Dhindsa <93422234+rdhkdh@users.noreply.github.com>
2026-04-09 16:22:13 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n<=2) {return 0;}
+
+        //prefix[i] = max height upto i (included)
+        //suffix[i] = max height upto i from behind 
+        vector<int> prefix(n); 
+        vector<int> suffix(n);
+        
+        prefix[0] = height[0];
+        for(int i=1; i<n; i++)
+        {
+            prefix[i] = max(prefix[i-1], height[i]);
+        }
+
+        suffix[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+            suffix[i] = max(suffix[i+1], height[i]);
+        }
+
+        int water_area = 0;
+        for(int i=1; i<=n-2; i++)
+        {   
+            int curr_water = min(prefix[i-1], suffix[i+1]) - height[i];
+            if(curr_water>0)
+            {
+                water_area += curr_water;
+            }
+        }
+
+        return water_area;
+    }
+};
